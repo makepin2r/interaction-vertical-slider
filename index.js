193bc08545f0f65ce5864a0d84f1940042b953a6
makepin2r <makepin2r@gmail.com>
2021-01-05 22:34:32 +0900

    슬라이드 높이에 맞게 이동 로직 수정.

--- index.js
+++ index.js
@@ -1,56 +1,71 @@
 // move-box move
+const slideHeight = 80;
 const box = document.querySelector('.move-box');
+const slides = document.querySelectorAll('.move-box li');
+const container = document.querySelector('.container');
 let startY = 0;
 let isUp = true;
 let threshold = 0;
 let isClicked = false;
+let currentIdx = 0;
 
-box.addEventListener('mousedown', (e) => {
+container.addEventListener('mousedown', (e) => {
     isClicked = true;
-
+    container.style.cursor = 'grab';
     // drag direction
     startY = e.pageY;
 })
 
-box.addEventListener('mouseup', (e) => {
+container.addEventListener('mouseup', (e) => {
     e.preventDefault();
+    moveSlide(currentIdx);
+    threshold = 0;
     isClicked = false;
-    console.log('mouse off!');
 })
-box.addEventListener('mouseleave', (e) => {
+container.addEventListener('mouseleave', (e) => {
     e.preventDefault();
+    moveSlide(currentIdx);
+    threshold = 0;
     isClicked = false;
-    console.log('mouse off!');
 })
 
-box.addEventListener('mousemove', (e) => {
+container.addEventListener('mousemove', (e) => {
     e.preventDefault();
     let moveY = e.pageY;
-    
     // distinguish drag direction
     if(isClicked) {
         if(startY < moveY) { isUp = false; }
         else { isUp = true; }
-        threshold += (moveY - startY); // 여기 계산이 잘못됐다
+        threshold += (moveY - startY);
         startY = moveY;
 
-        if(threshold >= 80 || threshold <= -80) {
-            moveElem(isUp);
+        if(threshold >= 80 && (currentIdx < slides.length - 1)){
+           currentIdx++; 
+           threshold = 0;
+           console.log('currentIdx: ' + currentIdx);
+        } else if (threshold <= -80 && currentIdx > 0) {
+            currentIdx--;
             threshold = 0;
-            return;
+            console.log('currentIdx: ' + currentIdx);
         }
     }
     
 })
 
-function moveElem(direction){
+function moveSlide(index){
     let offset = 0;
-    if(direction) { // is Up  
-        offset = (box.offsetTop - 80) + 'px';
-    } else { // is Down
-        offset = (box.offsetTop + 80) + 'px';
-    }
+    offset = `-${80*currentIdx}px`;
     box.style.top = offset;
+}
+
+function adjustOffset() {
+    let slide = 0;
+    if((box.offsetTop % slideHeight) !== 0) {
+        slide = box.offsetTop / slideHeight;
+        let offset = (slide * slideHeight) + 'px';
+        box.style.top = offset;
+        console.log('adjust:' + box.style.top);
+    }
 }
 
 init();
